73e2ef04e7698b65acf746a3a390b2f22ded69f5
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-06-07 02:40:37 +0300

    test using fluent assert

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+
+namespace NetworkUtility.Tests.PingTests;
+
+public class NetworkServiceTests
+{
+    [Fact]
+    public void NetworkService_SendPing_ReturnString()
+    {
+        // Arrange - variable, class, mocks
+        var pingService = new NetworkService();
+
+        // Act
+        var result= pingService.SendPing();
+
+        // Assert
+        result.Should().NotBeNullOrWhiteSpace();
+        result.Should().Be("Success: Ping Sent!");
+        result.Should().Contain("Success",Exactly.Once());
+    }
+
+    [Theory]
+    [InlineData(10,20,30)]
+    public void NetworkService_SetTimeout_ReturnSum(int a , int b, int expected)
+    {
+        // Arrange
+        var ping= new NetworkService();
+
+        // Act
+        var result=ping.SetTimeout(a,b);
+
+        // Assert
+        result.Should().Be(expected);
+        result.Should().NotBeInRange(-1000,0);
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,16 @@
+﻿namespace NetworkUtility.Ping;
+
+public class NetworkService
+{
+    public string SendPing()
+    {
+        // SearchDNS();
+        // BuildPacket();
+        return "Success: Ping Sent!";
+    }
+
+    public int SetTimeout(int a, int b)
+    {
+        return a + b;
+    }
+}
